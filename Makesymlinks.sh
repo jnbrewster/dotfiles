3964777e541989feb09e208b3b5ea5e27de74227
Joel Brewster <jnbrewster@fastmail.com>
2015-08-25 22:40:45 +0900

    Removed cruft

--- Makesymlinks.sh
+++ Makesymlinks.sh
@@ -6,7 +6,13 @@
 
 dir=~/Dropbox/dotfiles                    # dotfiles directory
 olddir=~/Dropbox/dotfiles_old             # old dotfiles backup directory
-files=" angband amethyst bash_profile bashrc config crontab hushlogin muttrc nvim nvimrc profile tmux.conf vim vimrc weechat . "    # list of files/folders to symlink in homedir
+files=" amethyst config muttrc nvim nvimrc profile tmux.conf vim vimrc weechat . "    # list of files/folders to symlink in homedir
+
+echo "Linking karabiner"
+ln -s ~/Dropbox/dotfiles/Karabiner/karabiner.xml ~/Library/Application\ Support/Karabiner/private.xml
+
+echo "linking Launchbar"
+ln -s ~/Dropbox/dotfiles/Launchbar/Configuration.plist ~/Library/Application\ Support/Launchbar/Configuration.plist
 
 # change to the dotfiles directory
 echo -n "Changing to the $dir directory ..."
