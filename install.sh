640a2a39d46fa633422034b31c2df6e4252874c9
Joel Brewster <jnbrewster@fastmail.com>
2015-09-08 13:10:35 +0900

    fix typo, add more packages

--- install.sh
+++ install.sh
@@ -1,27 +1,31 @@
 #!/usr/bin/env bash
 # Install command-line tools using Homebrew.
 
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)”≤
+ruby -e $"(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 brew update
-brew upgrade --all
 
 brew install tmux
-brew tap neovim/neovim
-brew install --HEAD neovim
 brew install weechat
 brew install profanity
 brew install cmus
+brew install fish
 brew install mutt
 brew install ranger
 brew cask install qlmarkdown
+brew install archey
+brew install homebrew/games/angband
+brew install mplayer
+brew install youtube-dl
 
+pip install glances
 
 #Install rails stuff
 brew install rbenv
 brew install ruby-build
 rbenv install 2.2.2
 rbenv global 2.2.2
+brew install heroku
 
 brew cleanup
 
@@ -35,19 +39,18 @@ brew install caskroom/cask/brew-cask
 apps=(
 	amethyst
   dropbox
+	firefox
   google-chrome
 	github-desktop
 	karabiner
 	launchbar
 	steam
-  flux
   vlc
   nvalt
 	r-name
 	sitesucker
   transmission
 	vimr
-	macvim
 )
 
 # Install apps to /Applications
@@ -65,6 +68,20 @@ echo "installing fonts..."
 brew cask install ${fonts[@]}
 
 brew update && brew upgrade brew-cask && brew cleanup && brew cask cleanup
+
+# Setup mac settings
+echo "setting up mac preferences..."
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
+chflags nohidden ~/Library
+defaults write com.apple.dock autohide -bool true
+defaults write NSGlobalDomain KeyRepeat -int 2
+defaults write NSGlobalDomain InitialKeyRepeat -int 15
+defaults write com.apple.finder WarnOnEmptyTrash -bool false
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
+sudo defaults write /Library/Preferences/.GlobalPreferences.plist _HIEnableThemeSwitchHotKey -bool true
+
+for app in Finder Dock SystemUIServer; do killall "$app" >/dev/null 2>&1; done
 
 cd ~
 echo "Finished!"
