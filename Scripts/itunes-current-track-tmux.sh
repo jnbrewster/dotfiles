9153d2a837c76b2340a2f66fd5862e09fc4174a2
Joel Brewster <jnbrewster@fastmail.com>
2015-09-09 19:13:14 +0900

    script updates?

--- Scripts/itunes-current-track-tmux.sh
+++ Scripts/itunes-current-track-tmux.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 ITUNES_TRACK=$(osascript <<EOF
-if appIsRunning("Spotify")
-    tell app "Spotify" to get the name of the current track
-else if appIsRunning("iTunes") then
+if appIsRunning("iTunes") then
     tell app "iTunes" to get the name of the current track
 end if
+
 on appIsRunning(appName)
     tell app "System Events" to (name of processes) contains appName
 end appIsRunning
@@ -12,15 +11,16 @@ EOF)
 
 if test "x$ITUNES_TRACK" != "x"; then
 ITUNES_ARTIST=$(osascript <<EOF
-if appIsRunning("Spotify")
-    tell app "Spotify" to get the artist of the current track
-else if appIsRunning("iTunes") then
+if appIsRunning("iTunes") then
     tell app "iTunes" to get the artist of the current track
 end if
+
 on appIsRunning(appName)
     tell app "System Events" to (name of processes) contains appName
 end appIsRunning
 EOF)
 
-    echo '#[fg=colour137,bg=colour234]⮂#[bg=colour137,fg=colour16,bold] ♫' $ITUNES_TRACK '#[nobold]by#[bold]' $ITUNES_ARTIST '#[fg=colour254,bg=colour137,nobold]'
+    echo '[▶' $ITUNES_ARTIST - $ITUNES_TRACK] ''
 fi
+
+
