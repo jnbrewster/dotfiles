af8ae360498819668b57037bc9fa1290f60ff618
Joel Brewster <joel@Sarang.local>
2015-10-27 11:11:53 +0900

    updated vim bindings in karabiner

--- karabiner.sh
+++ karabiner.sh
@@ -2,22 +2,34 @@
 
 cli=/Applications/Karabiner.app/Contents/Library/bin/karabiner
 
-$cli set remap.optionrcommandr 1
+$cli set remap.right_command_jk_to_scroll 1
 /bin/echo -n .
-$cli set remap.fkeys_to_consumer_f3_lion 1
+$cli set repeat.initial_wait 200
+/bin/echo -n .
+$cli set remap.fkeys_to_consumer_f5 1
 /bin/echo -n .
 $cli set remap.fkeys_to_consumer_f10 1
 /bin/echo -n .
 $cli set remap.fkeys_to_consumer_f1 1
 /bin/echo -n .
-$cli set remap.launcher_mode_v2 1
+$cli set remap.controlR2optionR 1
+/bin/echo -n .
+$cli set general.disable_internal_keyboard_if_external_keyboard_exsits 1
+/bin/echo -n .
+$cli set remap.optionrcommandr 1
 /bin/echo -n .
 $cli set remap.help2f13 1
 /bin/echo -n .
+$cli set remap.vimnormal_escape_toggle 1
+/bin/echo -n .
+$cli set remap.fkeys_to_consumer_f3_lion 1
+/bin/echo -n .
+$cli set remap.launcher_mode_v2 1
+/bin/echo -n .
 $cli set remap.fkeys_to_consumer_f7 1
 /bin/echo -n .
-$cli set remap.controlR2optionR 1
+$cli set remap.right_command_hl_to_scroll 1
 /bin/echo -n .
-$cli set remap.fkeys_to_consumer_f5 1
+$cli set remap.mouse_keys_mode_2 1
 /bin/echo -n .
 /bin/echo
