b8479abc290f82c8a348e7b8d5cfbccfe7b350c8
Joel <j.n.brewster@gmail.com>
2016-02-02 22:02:58 +0900

    Add Karabiner import script

---
+++ karabiner-import.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+cli=/Applications/Karabiner.app/Contents/Library/bin/karabiner
+
+$cli set remap.fkeys_to_consumer_f10 1
+/bin/echo -n .
+$cli set remap.fkeys_to_consumer_f3_lion 1
+/bin/echo -n .
+$cli set remap.fkeys_to_consumer_f1 1
+/bin/echo -n .
+$cli set remap.help2f13 1
+/bin/echo -n .
+$cli set remap.fkeys_to_consumer_f7 1
+/bin/echo -n .
+$cli set remap.fkeys_to_consumer_f5 1
+/bin/echo -n .
+/bin/echo
