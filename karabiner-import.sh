7ede264eeed2c8e5d3d21fc0fecb26ca493eb192
Joel Brewster <jnbrewster@fastmail.com>
2016-02-14 21:21:24 +0900

    added newsbeuter

--- karabiner-import.sh
+++ karabiner-import.sh
@@ -10,8 +10,6 @@ $cli set remap.fkeys_to_consumer_f1 1
 /bin/echo -n .
 $cli set remap.help2f13 1
 /bin/echo -n .
-$cli set remap.fkeys_to_consumer_f7 1
-/bin/echo -n .
 $cli set remap.fkeys_to_consumer_f5 1
 /bin/echo -n .
 /bin/echo
