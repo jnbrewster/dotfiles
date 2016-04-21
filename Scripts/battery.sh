a0e9ee7c81e2abdbae72c521de448190647bf279
Joel Brewster <jnbrewster@fastmail.com>
2016-04-21 17:48:24 +1000

    clean

--- Scripts/battery.sh
+++ Scripts/battery.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-if [ `uname` = 'Darwin' ] ; then
-  percent=`ioreg -l | grep -i capacity | tr '\n' ' | ' | awk '{printf("%d", $10/$5 * 100)}'`
-fi
-if   (( percent > 80  )); then color='#[fg=green]'
-elif (( percent > 65  )); then color='#[fg=yellow]'
-elif (( percent > 40  )); then color='#[fg=red]'
-else color='#[fg=white]'
-fi
-echo " #[nobright fg=white]« $color$percent% #[nobright fg=white]« "
+
+pmset -g batt | egrep '([0-9]+\%).*' -o --colour=auto | cut -f1 -d';'
+
