d566b7fbffbfafa9d81fa8dfb62618ea21819226
Joel Brewster <j.n.brewster@gmail.com>
2016-07-06 10:50:39 +1000

    Added son view

--- safari-extensions.bash
+++ safari-extensions.bash
@@ -5,3 +5,4 @@ rm $EXTENSIONPATH/*.safariextz
 
 $GRABEXTENSION "https://cache.agilebits.com/dist/1P/ext/1Password-4.2.5.safariextz"
 $GRABEXTENSION "https://safari-extensions.apple.com/extensions/com.instapaper.extension-CAM49M58WK/instapaper.safariextz"
+$GRABEXTENSION "https://github.com/acrogenesis/jsonview-safari/archive/v1.7.zip"
